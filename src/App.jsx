6858e7d05c5104fea4b66c809aac38e74ba5cc5d
Perico6255 <ruizcanduapedro@gmail.com>
2023-11-22 20:06:29 +0100

    try

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,10 @@
-import { Link, Route, Router, Routes } from 'react-router-dom'
-
-function Home() {
-  return <h2>Home</h2>
-}
-function About() {
-  return <h2>About</h2>
-}
-function Users() {
-  return <h2>Users</h2>
-}
 
 
 
 function App() {
   return (
     <>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/users">Users</Link>
-      </div>
-      <h1>App</h1>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/users" element={<Users />} />
-      </Routes>
-
+      <h1>Hello World</h1>
     </>
   )
 }
