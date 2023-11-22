cc5d177563d6a510ee2c2391a5f4d9a93e98d10c
Perico6255 <ruizcanduapedro@gmail.com>
2023-11-22 20:20:03 +0100

    react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
+import { Link, Route, Router, Routes } from 'react-router-dom'
+
+function Home() {
+  return <h2>Home</h2>
+}
+function About() {
+  return <h2>About</h2>
+}
+function Users() {
+  return <h2>Users</h2>
+}
 
 
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/users">Users</Link>
+      </div>
+      <h1>App</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
+
     </>
   )
 }
